2aba7efff186954def3fda85a369dbcc447f25e5
Andy Nowacki <a.nowacki@leeds.ac.uk>
2015-05-11 19:17:26 +0100

    split: Add best (phi,dt) to search_phi output, and add comments

--- split.jl
+++ split.jl
@@ -19,7 +19,9 @@ function search_phi(t1::SACtr, t2::SACtr;
 	for i = 1:nphi
 		dt, lam1[i,:], lam2[i,:] = search_dt(t1, t2, phi[i], dt_max, ndt)
 	end
-	return phi, dt, lam1, lam2
+	# Return minimum values as well
+	ip, idt = minloc2(lam2)
+	return phi, dt, lam1, lam2, phi[ip], dt[idt]
 end
 
 function search_dt(t1::SACtr, t2::SACtr, phi, dt_max, ndt)
@@ -34,8 +36,9 @@ function search_dt(t1::SACtr, t2::SACtr, phi, dt_max, ndt)
 end
 
 function apply_split!(t1::SACtr, t2::SACtr, phi::Number, dt::Number)
-	# Apply a splitting operator to two SAC traces.  They are assumed to be
-	# orthogonal, and the first trace should be anticlockwise of the second.
+	# Apply  a splitting operator to two SAC traces.  They are
+	# assumed to be orthogonal, and the first trace should be anticlockwise
+	# of the second.
 	theta = phi - t1.cmpaz
 	SAC.rotate_through!(t1, t2, theta)
 	# Move the fast trace forward
@@ -47,6 +50,8 @@ end
 function compute_eigvals(t1::SACtr, t2::SACtr, phi::Number, dt::Number)
 	# Compute the two eigenvalues for traces for a specific fast orientation, phi,
 	# and delay time, dt.
+	# Note that we swap the delay time, because we are applying the inverse
+	# splitting operator.
 	T1, T2 = SAC.copy(t1), SAC.copy(t2)
 	apply_split!(T1, T2, phi, -dt)
 	c = covar(T1, T2)
@@ -61,6 +66,28 @@ function covar(t1::SACtr, t2::SACtr)
 	c[2,2] = sum(t2.t.^2)
 	c[1,2] = c[2,1] = sum(t1.t.*t2.t)
 	return c
+end
+
+function minloc2(A)
+	# Return an 2-tuple which contains the location of the minimum
+	# value of a 2-dimensional array.
+	# Why isn't this implemented (in general) for n-arrays in Julia already?
+	imin = jmin = 0
+	for j = 1:size(A, 2)
+		for i = 1:size(A, 1)
+			if i == j == 1
+				imin = jmin = 1
+				min = A[i,j]
+			else
+				if A[i,j] < min
+					imin = i
+					jmin = j
+					min = A[i,j]
+				end
+			end
+		end
+	end
+	return (imin,jmin)
 end
 
 end # module
