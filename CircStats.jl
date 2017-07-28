6a417203a037532064d61fafb58f243d90c077ed
Andy Nowacki <a.nowacki@leeds.ac.uk>
2017-07-28 16:33:16 +0100

    CircStats: Update `cmedian`
    - Add dots for deprecated functions on arrays.
    - Add arguments for axial data and degrees.

--- CircStats.jl
+++ CircStats.jl
@@ -40,16 +40,18 @@ Angles are in radians, unless `degrees` == true.
 cmean(a) = atan2(sum(sin.(a)), sum(cos.(a)))
 cmean(a, degrees::Bool) = degrees ? rad2deg(cmean(deg2rad.(a))) : cmean(a)
 
-"`cmedian(a::Array, degrees::Bool=false) -> median`
+"`cmedian(a::Array, degrees::Bool=false, axial=false) -> median`
 
 Return the median angle from the set of angles `a`.
 
 Angles are in radians, unless `degrees` == true.
 "
-function cmedian(a)
-    medians = Float64[]
+function cmedian(a, degrees::Bool=true, axial::Bool=false)
+    medians = Vector{eltype(a)}()
     A = sort(a)
     n = length(A)
+    degrees && (A .= deg2rad.(A))
+    axial && (A .= 2A)
     # Compute trial bisectors, which are either the points themselves or adjacent means
     p = Array{Float64}(n)
     if iseven(n)
@@ -58,17 +60,17 @@ function cmedian(a)
             p[i] = cmean([A[i], A[j]])
         end
     else
-        p[:] = A[:]
+        p[:] .= A[:]
     end
     # Try all possible diameters
     for i = 1:n
         # Count points on either side of diameter
-        n_plus = sum(cdist(p[i], A) .> 0)
-        n_minus = sum(cdist(p[i], A) .< 0)
+        n_plus = sum(cdist.(p[i], A) .> 0)
+        n_minus = sum(cdist.(p[i], A) .< 0)
         if n_plus == n_minus
             # Determine which side of circle is correct direction by counting the number
             # within pi/2 of each of the two opposing possible medians
-            if sum(abs(cdist(p[i], A)) .<= pi/2) > sum(abs(cdist(p[i], A)) .> pi/2)
+            if sum(abs.(cdist.(p[i], A)) .<= pi/2) > sum(abs.(cdist.(p[i], A)) .> pi/2)
                 push!(medians, A[i])
             else
                 push!(medians, (A[i] + 2pi)%2pi - pi)
@@ -77,13 +79,17 @@ function cmedian(a)
     end
     # If there is more than one median, take the mean thereof (Otieno & Anderson-Cook
     # (2003), Journal of Modern Applied Statistical Methods, 2(1), 168-176)
-    if length(medians) > 1
+    median = if length(medians) > 1
         cmean(medians)
-    else
+    elseif length(medians) == 1
         medians[1]
+    else
+        error("Zero medians found.  Are data axial but axial!=true?")
     end
+    degrees && (median = rad2deg(median))
+    axial && (median /= 2)
+    median
 end
-cmedian(a, degrees::Bool) = degrees ? rad2deg(cmedian(deg2rad(a))) : cmedian(a)
 
 "Example sorted angle data, quoted in Otieno & Anderson-Cook (2003)"
 frog_data = Float64[104, 110, 117, 121, 127, 130, 136, 144, 152, 178, 184, 192, 200, 316]
