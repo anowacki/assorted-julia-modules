bda8a42da821b700ea2fe3226efa109dd0869520
Andy Nowacki <a.nowacki@leeds.ac.uk>
2017-09-06 10:41:06 +0100

    PSH: Change implementation to follow Kennett more closely
    Previous implementation had a fudge which doesn't seem
    to have any basis in physics.  The branch `psh-update`
    is to test this.

--- PSH.jl
+++ PSH.jl
@@ -8,24 +8,30 @@ module PSH
 
 using SAC
 
-export psh!
+export psh, psh!
 
 """
-    psh_matrix!(α, β, p, M)
+    psh_matrix!(α, β, p, M) -> M
 
 Fill in the PSH transfer matrix `M`, given the P-wave velocity `α`, S-wave velocity `β`
-and the slowness `p`.  Velocities are in km/s, slowness in s/°
+and the slowness `p`.  Velocities are in km/s, slowness in s/°.
 """
 function psh_matrix!{T}(a, b, p, M::AbstractArray{T,2})
     size(M) == (3,3) || error("Transfer matrix must have size (3,3)")
-    pr = p/111.
+    p /= 111.1 # Convert to s/km
     isevanescent(a, b, p) &&
-		error("psh_matrix!: Evanescent wave not permitted (vp=$a, vs=$b, p=$p)")
-    qa = sqrt(1/a^2 - pr^2)
-    qb = sqrt(1/b^2 - pr^2)
-    M[:,:] .= [pr*b^2/a              0   (b^2*pr^2-0.5)/(a*qa);
-               (0.5-b^2*pr^2)/(b*qb) 0   pr*b;
-               0                    0.5  0                    ]
+        warn("psh_matrix!: Evanescent wave (vp=$a, vs=$b, p=$p)")
+    qa = sqrt(1/a^2 - p^2)
+    qb = sqrt(1/b^2 - p^2)
+    # Kennett, GJI, 1991
+    Vpz = -(1 - 2b^2*p^2)/(2a*qa)
+    Vpr = p*b^2/a
+    Vsz = p*b
+    Vsr = (1 - 2b^2*p^2)/(2b*qb)
+    Vht = 1/2
+    M[:,:] .= [Vpz Vsz  0 
+               Vpr Vsr  0 
+                0   0  Vht]
 end
 
 """
@@ -34,11 +40,8 @@ end
 Return the PSH transfer matrix M, given the P-wave velocity `α`, S-wave velocity `β`
 and the slowness `p`.  Velocities are in km/s, slowness in s/°
 """
-function psh_matrix(a, b, p)
-    M = Array{Float64}(3, 3)
-    psh_matrix!(a, b, p, M)
-    M
-end
+psh_matrix(a, b, p) = psh_matrix!(a, b, p, Array{Complex{Float64}}(3,3))
+
 
 """
     isevanescent(α, β, p) -> ::Bool
@@ -46,33 +49,38 @@ end
 Return true if the combination of Vp `α`, Vs `β` and slowness `p` lead to an evanescent
 wave at the surface.
 """
-isevanescent(a, b, p) = 1/a^2 - (p/111.)^2 <= 0 || 1/b^2 - (p/111.)^2 <= 0
+function isevanescent(a, b, p)
+    p = Float64(p/111.)
+    a, b = Float64(a), Float64(b)
+    1/a^2 - p^2 <= 0.0 || 1/b^2 - p^2 <= 0.0
+end
 
 
 """
     psh!(Z, R, T, α, β, p) -> P, S, H
 
 Converts three arrays, `SACtr`s or arrays of `SACtr`s in ZRT orientation into
-PSH orientation, using the supplied Vp `α`, Vs `β` (km/s) and slowness `p` (s/km).
+PSH orientation, using the supplied Vp `α`, Vs `β` (km/s) and slowness `p` (s/°).
 
 Traces are updated to be in the order P (like vertical), S (like radial), H (like transverse).
 """
 function psh!{F}(Z::Array{F,1}, R::Array{F,1}, T::Array{F,1}, a, b, p)
 	length(Z) == length(R) == length(T) || error("psh!: Traces must be same length")
 	M = psh_matrix(a, b, p)
-    A = M*[R'; T'; Z']
-    Z[:] = vec(A[1,:])
-    R[:] = vec(A[2,:])
-    T[:] = vec(A[3,:])
+    # Kennett 1991
+    Z, R, T = M[1,1]*Z + M[1,2]*R + M[1,3]*T,
+              M[2,1]*Z + M[2,2]*R + M[2,3]*T,
+              M[3,1]*Z + M[3,2]*R + M[3,3]*T
     Z, R, T
 end
 function psh!(Z::SACtr, R::SACtr, T::SACtr, a, b, p)
     psh!(Z.t, R.t, T.t, a, b, p)
     for t in (Z, R, T) SAC.update_headers!(t) end
+    Z.kcmpnm, R.kcmpnm, T.kcmpnm = "P", "S", "H"
     Z, R, T
 end
 function psh!(Z::Array{SACtr}, R::Array{SACtr}, T::Array{SACtr}, a, b, p)
-    for (z, r, t) in zip(Z, R, T) psh!(z, r, t) end
+    for (z, r, t) in zip(Z, R, T) psh!(z, r, t, a, b, p) end
     Z, R, T
 end
 
@@ -80,7 +88,7 @@ end
     psh(Z, R, T, α, β, p) -> P, S, H
 
 Convert three arrays, `SACtr`s or arrays of `SACtr`s in ZRT orientation into
-PSH orientation, using the supplied Vp `α`, Vs `β` (km/s) and slowness `p` (s/km),
+PSH orientation, using the supplied Vp `α`, Vs `β` (km/s) and slowness `p` (s/°),
 where `P` is parallel to P-wave motion positive away from the source, `S` is parallel
 to SV and `H` is the transverse direction.
 """
